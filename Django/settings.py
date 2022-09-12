60494c40fedb0dd94ee9ee26e50d7e08e0b1d353
Abu Bakkar Siddik <emichelkhan@gmail.com>
2022-09-13 00:33:37 +0600

    create database
    create database in postgres using help of python class. and add super user. and register database in admin panel

--- Django/settings.py
+++ Django/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'travello'
+    'travello',
+
 ]
 
 MIDDLEWARE = [
@@ -79,8 +80,12 @@ WSGI_APPLICATION = 'Django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'travello',
+        'USER': 'postgres',
+        'PASSWORD': '198803emon',
+        'HOST': 'localhost'
+
     }
 }
 
