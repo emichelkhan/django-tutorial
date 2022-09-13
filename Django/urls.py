116881b2bbcfca1a6c0a3d893db45fe1d10328b2
Abu Bakkar Siddik <emichelkhan@gmail.com>
2022-09-13 18:29:24 +0600

    creating signup page
    creating signup page for registration

--- Django/urls.py
+++ Django/urls.py
@@ -5,7 +5,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('travello.urls'))
+    path('', include('travello.urls')),
+    path('accounts/', include('accounts.urls'))
 
 ]
 
