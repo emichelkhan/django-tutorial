e076ed57ce304f5697fa910e6a5020975b238857
Abu Bakkar Siddik <emichelkhan@gmail.com>
2022-09-13 23:41:47 +0600

    crate a sign up page
    create a sign up page for user sign up.

--- Django/settings.py
+++ Django/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-r=xae##7)o-1#jz^g5a#ohw#85_8fx6g4or@=l+76rm=dni&m#
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -59,7 +57,6 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR / 'templates']
 
-
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -75,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Django.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -89,7 +85,6 @@ DATABASES = {
 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -109,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -120,7 +114,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 
 
 # Create your views here.
 
 def register(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
 
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Already Exists')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, password=password1, email=email,
+                                                first_name=first_name,
+                                                last_name=last_name)
+                user.save()
+        else:
+            messages.info(request, 'Password not matching')
+            return redirect('register')
+        return redirect('/')
 
+    else:
+        return render(request, 'signup.html')
