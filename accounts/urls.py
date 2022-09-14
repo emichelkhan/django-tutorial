35ce03587ff2ea17b22b6e6ff6463979bdd4477f
Abu Bakkar Siddik <emichelkhan@gmail.com>
2022-09-14 21:44:10 +0600

    login created

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,9 @@
-
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register', views.register, name='register'),
-    path('login', views.signin, name='login')
-    ]
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('terms', views.terms, name='terms'),
+    path('signout', views.signout, name='sign-out')
+]
--- accounts/views.py
+++ accounts/views.py
@@ -9,9 +9,7 @@ def signin(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-
         user = auth.authenticate(username=username, password=password)
-
         if user is not None:
             auth.login(request, user)
             return redirect('/')
@@ -22,7 +20,7 @@ def signin(request):
         return render(request, 'signin.html')
 
 
-def register(request):
+def signup(request):
     if request.method == 'POST':
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
@@ -30,24 +28,30 @@ def register(request):
         email = request.POST['email']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
-
         if password1 == password2:
             if User.objects.filter(username=username).exists():
                 messages.info(request, 'Username Taken')
-                return redirect('register')
+                return redirect('signup')
             elif User.objects.filter(email=email).exists():
-                messages.info(request, 'Email Already Exists')
-                return redirect('register')
+                messages.info(request, 'Email Taken')
+                return redirect('signup')
             else:
                 user = User.objects.create_user(username=username, password=password1, email=email,
-                                                first_name=first_name,
-                                                last_name=last_name)
+                                                first_name=first_name, last_name=last_name)
                 user.save()
                 return redirect('signin')
         else:
             messages.info(request, 'Password not matching')
-            return redirect('register')
-        return redirect('/')
+            return redirect('signup')
 
     else:
         return render(request, 'signup.html')
+
+
+def terms(request):
+    return render(request, 'terms.html')
+
+
+def signout(request):
+    auth.logout(request)
+    return redirect('/')
